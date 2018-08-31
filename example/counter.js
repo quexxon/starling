79066f0c0553363b4cbd5bf85a3651ae265732d8
Will Clardy <wclardy@kmbs.konicaminolta.us>
2018-08-31 15:06:15 -0400

    Update sample application with more example usage.

--- example/counter.js
+++ example/counter.js
@@ -7,13 +7,14 @@ const newAction = starling.newAction;
 
 const initialState = {
     counter: 0,
+    step: 1,
 };
 
 //-------------------------------------------------------------------------------
 // INIT
 //-------------------------------------------------------------------------------
 
-const init = () => {
+const init = (_environment) => {
     return {
         diff: initialState,
         tasks: [],
@@ -28,13 +29,25 @@ const update = (state, action) => {
     switch (action.msg) {
         case 'DECREMENT':
             return {
-                diff: { counter: state.counter - 1 },
+                diff: { counter: state.counter - state.step },
                 tasks: [],
             };
 
         case 'INCREMENT':
             return {
-                diff: { counter: state.counter + 1 },
+                diff: { counter: state.counter + state.step },
+                tasks: [],
+            };
+
+        case 'SET_STEP':
+            return {
+                diff: { step: parseInt(action.data.step) },
+                tasks: [],
+            };
+
+        case 'RESET':
+            return {
+                diff: initialState,
                 tasks: [],
             };
 
@@ -49,9 +62,30 @@ const update = (state, action) => {
 
 const view = (state) => {
     return h('div', {}, [
+        counter(state),
+        stepInput(state),
+        h('button', { onclick: () => newAction('RESET') }, 'Reset'),
+    ]);
+};
+
+const counter = (state) => {
+    return h('div', { className: 'counter' }, [
         h('button', { onclick: () => newAction('DECREMENT') }, '-'),
         h('span', {}, state.counter),
         h('button', { onclick: () => newAction('INCREMENT') }, '+'),
+    ]);
+};
+
+const stepInput = (state) => {
+    return h('label', {}, [
+        h('span', {}, 'Step:'),
+        h('input', {
+            type: 'number',
+            value: state.step,
+            oninput: (e) => {
+                newAction('SET_STEP', { step: e.target.value });
+            }
+        }, []),
     ]);
 };
 
@@ -59,5 +93,11 @@ const view = (state) => {
 // BOOTSTRAP
 //-------------------------------------------------------------------------------
 
+
+// Use `environment` for external state that you'd like to provide to the
+// application when `init` is called. The `environment` argument to `embed` is
+// optional and is provided here solely as documentation. The default value is
+// `{}`.
+const environment = {};
 const root = document.getElementById('app');
-starling.createApp(init, update, view).embed(root);
+starling.createApp(init, update, view).embed(root, environment);
